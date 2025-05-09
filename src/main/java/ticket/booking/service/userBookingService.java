12965813f98bbe44e0f27b7b70623e31e01c8fd1
Saurav_Bhasin <bhasinsaurav9@gmail.com>
2025-05-08 23:43:22 -0400

    Entities created

---
+++ src/main/java/ticket/booking/service/userBookingService.java
@@ -0,0 +1,14 @@
+package ticket.booking.service;
+
+import ticket.booking.entities.User;
+
+public class userBookingService {
+
+    private User user;
+
+    public userBookingService(User user){
+        this.user = user;
+    }
+
+    
+}
