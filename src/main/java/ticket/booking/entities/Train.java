30a4261699cdb9f537569ba596f6093ba001a567
Saurav_Bhasin <bhasinsaurav9@gmail.com>
2025-05-11 17:31:09 -0400

    Login functionality added

--- src/main/java/ticket/booking/entities/Train.java
+++ src/main/java/ticket/booking/entities/Train.java
@@ -8,7 +8,59 @@ public class Train {
     private String trainId;
     private String trainNo;
     private List<List<Integer>> seats;
-    private Map<String, Time> stationTime;
+    private Map<String, String> stationTime;
     private List<String> train;
+
+    public Train(String trainId, String trainNo, List<List<Integer>> seats, Map<String, String> stationTimes, List<String> train){
+
+        this.seats = seats;
+        this.train = train;
+        this.stationTime = stationTimes;
+        this.trainNo = trainNo;
+        this.trainId = trainId;
+    }
+    public String getTrainId() {
+        return trainId;
+    }
+
+    public String getTrainNo() {
+        return trainNo;
+    }
+
+    public List<List<Integer>> getSeats() {
+        return seats;
+    }
+
+    public List<String> getTrain() {
+        return train;
+    }
+
+    public void setTrain(List<String> train) {
+        this.train = train;
+    }
+
+    public Map<String, String> getStationTime() {
+        return stationTime;
+    }
+
+    public void setStationTime(Map<String, String> stationTime) {
+        this.stationTime = stationTime;
+    }
+
+    public void setSeats(List<List<Integer>> seats) {
+        this.seats = seats;
+    }
+
+    public void setTrainNo(String trainNo) {
+        this.trainNo = trainNo;
+    }
+
+    public void setTrainId(String trainId) {
+        this.trainId = trainId;
+    }
+
+    public String getTrainInfo(){
+        return String.format("Train ID: %s Train No: %s", trainId, trainNo);
+    }
 
 }
--- src/main/java/ticket/booking/service/userBookingService.java
+++ src/main/java/ticket/booking/service/userBookingService.java
@@ -1,14 +1,54 @@
 package ticket.booking.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
+import ticket.booking.util.userServiceUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 public class userBookingService {
 
     private User user;
 
-    public userBookingService(User user){
+    private List<User> userList;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String userPath = "src/main/java/localDb/users.json";
+
+    public userBookingService(User user) throws IOException {
         this.user = user;
+        File users = new File(userPath);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
     }
 
-    
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equalsIgnoreCase(user.getName()) && userServiceUtil.checkPassword(user.getPassword(), user1.getHashPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch(IOException ex){
+            return Boolean.FALSE;
+
+        }
+    }
+
+    public void saveUserListToFile() throws IOException {
+        File usersFile = new File(userPath);
+        objectMapper.writeValue(usersFile, userList);
+    }
+    // json --> Object is called deserialization
+    // Object (User) --> json --> Serialize
+
 }
